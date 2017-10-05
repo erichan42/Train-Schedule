320e0d34ddf1ddbcd146ae00bb6c6481f235462e
Mus-tard <erichan210hh@gmail.com>
2017-10-05 13:13:03 -0700

    final push

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var config = {
     storageBucket: "",
     messagingSenderId: "810675081150"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -13,13 +14,11 @@ var database = firebase.database();
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
-  // Grabs user input
   var trainName = $("#train-name-input").val().trim();
   var trainDestination = $("#destination-input").val().trim();
   var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
   var trainRate = $("#rate-input").val().trim();
 
-  // Creates local "temporary" object for holding employee data
   var newTrain = {
     name: trainName,
     destination: trainDestination,
@@ -27,71 +26,41 @@ $("#add-train-btn").on("click", function(event) {
     rate: trainRate
   };
 
-  // Uploads employee data to the database
   database.ref().push(newTrain);
 
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.role);
-  console.log(newTrain.start);
-  console.log(newTrain.rate);
-
-  // Alert
-  alert("Train successfully added");
-
-  // Clears all of the text-boxes
   $("#train-name-input").val("");
   $("#destination-input").val("");
   $("#start-input").val("");
   $("#rate-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
 
-  // Store everything into a variable.
   var trainName = childSnapshot.val().name;
   var trainDestination = childSnapshot.val().destination;
   var trainStart = childSnapshot.val().start;
   var trainRate = childSnapshot.val().rate;
 
-  // Prettify the employee start
   var trainStartPretty = moment.unix(trainStart).format("HH:mm");
 
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-  // Assumptions
-    var tFrequency = trainRate;
+  var tFrequency = trainRate;
 
-    var firstTime = trainStartPretty;
+  var firstTime = trainStartPretty;
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+  var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
 
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+  var currentTime = moment();
+  
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  
+  var tRemainder = diffTime % tFrequency;
+  
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm");
 
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-
-  // Add each train's data into the table
   $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
   trainRate + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 });
